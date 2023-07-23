53a4ac9bccec089adf2be7e4cf7cb1336a547d8d
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-07-23 22:24:23 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31, num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 4, num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(10, 5, 4, 20);
+console.log(max)
